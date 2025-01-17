704824abd61a3ac2ba2d0f28a3a76c492854bc8d
jandavi <rodriguesjandavi193@gmail.com>
2025-01-17 10:39:09 -0300

    adicionando arquivos de css para o front-end

--- Sistema_de_Agendamentos/SISTEMA/SISTEMA/settings.py
+++ Sistema_de_Agendamentos/SISTEMA/SISTEMA/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# URL para acessar arquivos estáticos no navegador
+STATIC_URL = '/static/'
+
+# Diretório onde os arquivos estáticos serão coletados
+STATIC_ROOT = BASE_DIR / 'staticfiles'
